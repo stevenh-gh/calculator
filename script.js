0810b99eb58a271683db4bf45ba52f2d38d719fb
steven huynh <steven.h@live.com>
2020-10-19 15:05:53 -0700

    added operate function

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+const operate = (operator, a, b) => {
+	switch (operator) {
+		case 'add': return add(a, b);
+		case "subtract": return subtract(a, b);
+		case "multiply": return multiply(a, b);
+		case "divide": return divide(a, b);
+	}
+};
