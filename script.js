a9bee494d5c6ca81e0ae476369c340eb490ccb10
steven huynh <steven.h@live.com>
2020-10-20 20:45:16 -0700

    added chaining

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 const operate = (a, operator, b) => {
-	a = Number(a);
-	b = Number(b);
-	switch (operator) {
-		case 'add': return add(a, b);
-		case 'subtract': return subtract(a, b);
-		case 'multiply': return multiply(a, b);
-		case 'divide': return divide(a, b);
-	}
+        a = Number(a);
+        b = Number(b);
+
+        // if (operator === 'divide' && b === 0) { return 'YOU CAN\'T DIVIDE BY ZERO'; }
+
+        switch (operator) {
+                case 'add': return add(a, b);
+                case 'subtract': return subtract(a, b);
+                case 'multiply': return multiply(a, b);
+                case 'divide': return divide(a, b);
+        }
 };
 
 const btnContainer = document.querySelector('#btn-container');
@@ -26,44 +29,44 @@ let queue = [];
 let answer;
 
 buttons.forEach(button => {
-	button.addEventListener('click', e => {
-		let btnClass = e.target.parentElement.classList[1];
+        button.addEventListener('click', e => {
+                let btnClass = e.target.parentElement.classList[1];
 
+                if (btnClass === 'operand') {
+                        if (isPrevClickOperator) {
+                                console.log('in prevclick');
+                                input.value = '';
+                                isPrevClickOperator = false;
+                        }
+                        if (input.value.length !== 9) {
+                                if (input.value !== '0') {
+                                        input.value += e.target.value;
+                                } else { input.value = e.target.value; }
+                        }
+                } else if (btnClass === 'operator') {
+                        isPrevClickOperator = true;
+                        console.log(e.target.value);
+                        queue.push(input.value);
+                        queue.push(e.target.value);
+                        console.log(queue);
 
-		// if (queue.length === 3) {
-		// 	answer = operate(queue[0], queue[1], queue[2]);
-		// 	queue = [];
-		// }
-
-		if (btnClass === 'operand') {
-			if (isPrevClickOperator) {
-				console.log('in prevclick');
-				input.value = '';
-				isPrevClickOperator = false;
-			}
-			if (input.value.length !== 9) {
-				if (input.value !== '0') {
-					input.value += e.target.value;
-				} else { input.value = e.target.value; }
-			}
-		} else if (btnClass === 'operator') {
-			isPrevClickOperator = true;
-			console.log(e.target.value);
-			queue.push(input.value);
-			queue.push(e.target.value);
-			console.log(queue);
-		} else if (btnClass === 'equals') {
-			queue.push(input.value);
-			console.log(queue);
-			answer = operate(queue[0], queue[1], queue[2]);
-			input.value = answer;
-			queue = [];
-			console.log(e.target.value);
-			isPrevClickOperator = true;
-		} else if (btnClass === 'clear') {
-			input.value = '0';
-			queue = [];
-			console.log(e.target.value);
-		}
-	});
+                        if (queue.length > 3) {
+                                answer = operate(queue[0], queue[1], queue[2]);
+                                input.value = answer;
+                                queue = [answer, e.target.value];
+                        }
+                } else if (btnClass === 'equals') {
+                        queue.push(input.value);
+                        console.log(queue);
+                        answer = operate(queue[0], queue[1], queue[2]);
+                        input.value = answer;
+                        queue = [];
+                        console.log(e.target.value);
+                        isPrevClickOperator = true;
+                } else if (btnClass === 'clear') {
+                        input.value = '0';
+                        queue = [];
+                        console.log(e.target.value);
+                }
+        });
 });
